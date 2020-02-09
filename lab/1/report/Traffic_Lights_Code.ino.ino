7edf1de5707e13b7304aa1fcfeeae52d45836dd0
HeytemBoumazaIotProject <60180129+HeytemBoumazaIotProject@users.noreply.github.com>
2020-02-09 18:58:32 +0100

    updated comments

--- lab/1/report/Traffic_Lights_Code.ino.ino
+++ lab/1/report/Traffic_Lights_Code.ino.ino
@@ -2,7 +2,7 @@ const int LED2_Green = 2;
 const int LED5_Yellow = 5;
 const int LED10_Red = 10;
  
-// the setup routine runs once when you press reset:
+
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(LED2_Green, OUTPUT);   
@@ -10,19 +10,19 @@ void setup() {
   pinMode(LED10_Red,OUTPUT);
 }
  
-// the loop routine runs over and over again forever:
+
 void loop() {
-  digitalWrite(LED2_Green, HIGH);      // turn the LED on (HIGH is the voltage level)
-  delay(4000);                  // wait for a second
-  digitalWrite(LED2_Green, LOW);       // turn the LED off by making the voltage LOW
+  digitalWrite(LED2_Green, HIGH);      // turn the green LED on
+  delay(4000);                  // wait for  4 seconds
+  digitalWrite(LED2_Green, LOW);       // turn the green LED off by making the voltage LOW
   
 
-  digitalWrite(LED5_Yellow, HIGH);      // turn the LED on (HIGH is the voltage level)
-  delay(3000);                  // wait for a second
-  digitalWrite(LED5_Yellow, LOW);       // turn the LED off by making the voltage LOW
+  digitalWrite(LED5_Yellow, HIGH);      // turn the yellow LED on
+  delay(3000);                  // wait for 3 seconds
+  digitalWrite(LED5_Yellow, LOW);       // turn the yellow LED off by making the voltage LOW
   
-  digitalWrite(LED10_Red, HIGH);      // turn the LED on (HIGH is the voltage level)
-  delay(5000);                  // wait for a second
-  digitalWrite(LED10_Red, LOW);       // turn the LED off by making the voltage LOW
+  digitalWrite(LED10_Red, HIGH);      // turn the red LED on
+  delay(5000);                  // wait for 5 seconds
+  digitalWrite(LED10_Red, LOW);       // turn the red LED off by making the voltage LOW
   
 }
