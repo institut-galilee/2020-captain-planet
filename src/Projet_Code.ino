28ba45f9e98113b272edb9f4f3e3482266a7a43f
HeytemBoumazaIotProject <60180129+HeytemBoumazaIotProject@users.noreply.github.com>
2020-04-08 16:43:51 +0100

    MQ135 calibration
    added the method required to convert raw sensor reading to ppm

--- src/Projet_Code.ino
+++ src/Projet_Code.ino
@@ -4,6 +4,12 @@
  * 
  * 
  */
+/*Code de projet IOT , Groupe captain_planet
+ * 
+ * 
+ * 
+ * 
+ */
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include "Arduino.h"
@@ -11,8 +17,8 @@
 #include <ThingSpeak.h>
 
 //Parametres de connexion avec ThingSpeak
-const char * API_KEY = "BA8YEDX40DJHOP0M"; 
-unsigned long CHANNEL = 1015166 ;                 
+const char * API_KEY = "H8FPZCNB49A0MNIT"; 
+unsigned long CHANNEL = 1022022 ;                 
 const char *ssid =  "HONOR 20 Lite";                                    
 const char *pass =  "HonorableThief";
 const char* server = "api.thingspeak.com";
@@ -20,10 +26,15 @@ WiFiClient client;
 
 //Les pins
  #define DHTPIN 14
- int redPin = 27;
- int greenPin = 26;
- int bluePin = 25;
+ int redPin = 5;
+ int greenPin = 3;
+ int yellowPin =18 ;
  int D33 = 33;
+ int buzzerPin=18;
+
+ //les constants
+ float m = -0.3376; //Slope 
+ float b = 0.7165; //Y-Intercept 
 
 #define DHTTYPE DHT22   // DHT 11 
 
@@ -35,10 +46,13 @@ void setup() {
   //RGB pins
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
 
   //analog input pin for the mq135
   pinMode(D33,INPUT);
+
+  //input pin for the buzzer
+  pinMode(buzzerPin,OUTPUT);
   
   
   Serial.begin(9600);
@@ -50,11 +64,10 @@ void setup() {
 }
 
 void loop() {
-  float t = readTemperatureDHT22();
-  float h = readHumidityDHT22();
-  int x = analogRead(D33);
-  Serial.println(x);
-  ReadMQ135Data();
+  digitalWrite(redPin,LOW);
+  digitalWrite(yellowPin,LOW);
+  digitalWrite(greenPin,LOW);
+  //digitalWrite(buzzerPin, HIGH);
   
 /*
  * Connexion au plateforme thingspeak
@@ -69,13 +82,25 @@ void loop() {
     }
     Serial.println("\nConnected.");
   }
+    readTemperatureAndHumidity();
+    float t = readTemperatureDHT22();
+    float h = readHumidityDHT22();
+    /*int x = analogRead(D33);
+    Serial.println(x);*/
+    float a = readPPMData();
+
+     
+
+   /* ThingSpeak.setField(1, a);
+    ThingSpeak.setField(2, t);
+    ThingSpeak.setField(3, h);*/
     
-  
-    ThingSpeak.setField(1, t);
-    ThingSpeak.setField(2, h);
-   //int writeField(unsigned long channelNumber, unsigned int field, float value, const char * writeAPIKey)
-   int tempRes = ThingSpeak.writeField(CHANNEL,1,t,API_KEY);
-   int humRes = ThingSpeak.writeField(CHANNEL,2,h,API_KEY);
+    int airRes = ThingSpeak.writeField(CHANNEL,1,a,API_KEY);
+    delay(20000);
+    int tempRes = ThingSpeak.writeField(CHANNEL,2,t,API_KEY);
+    delay(20000);
+    int humRes = ThingSpeak.writeField(CHANNEL,3,h,API_KEY);
+    delay(20000);
     
     // Write to the ThingSpeak channel
      
@@ -84,16 +109,22 @@ void loop() {
       Serial.println("temperature updated");
     }
     else {
-      Serial.println("Problem updating temperature channel. HTTP error code " + String(tempRes));
+      Serial.println("Problem updating temperature field. HTTP error code " + String(tempRes));
     }
    if (humRes == 200) {
       Serial.println("humidity updated");
     }
     else {
-      Serial.println("Problem updating humdity channel. HTTP error code " + String(humRes));
+      Serial.println("Problem updating humdity field. HTTP error code " + String(humRes));
     }
 
-    delay(30000);
+    if (airRes == 200){
+      Serial.println("air quality updated");
+    }else {
+      Serial.println("Problem updating air quality field. HTTP error code " + String(airRes));
+    }
+
+    delay(10000);
     
     
   
@@ -143,16 +174,22 @@ void readTemperatureAndHumidity(){
   Serial.println("F");
 }
 
-void lightRedRGB(){
-digitalWrite(redPin,HIGH);
+void greenFlash(){
+ digitalWrite(redPin,LOW);
+ digitalWrite(yellowPin,LOW);
+ digitalWrite(greenPin,HIGH);
   
 }
-void lightYellowRGB(){
- digitalWrite(redPin,HIGH);
- digitalWrite(greenPin,HIGH);  
+void yellowFlash(){
+ digitalWrite(redPin,LOW);
+ digitalWrite(greenPin,LOW);
+ digitalWrite(yellowPin,HIGH);
+ 
 }
-void lightGreenRGB(){
-digitalWrite(greenPin,HIGH);  
+void redFlash(){
+digitalWrite(yellowPin,LOW);
+ digitalWrite(greenPin,LOW);
+ digitalWrite(redPin,HIGH);
 }
 
 
@@ -166,7 +203,7 @@ float readTemperatureDHT22(){
   return t ;
 }
 
-void ReadMQ135Data(){
+  float calculateR0(){
   float sensor_volt; //Define variable for sensor voltage 
   float RS_air; //Define variable for sensor resistance
   float R0; //Define variable for R0
@@ -187,5 +224,45 @@ void ReadMQ135Data(){
  
   Serial.print("R0 = "); //Display "R0"
   Serial.println(R0); //Display value of R0 
-  delay(1000); //Wait 1 second 
+  return R0 ;
 }
+
+
+float readPPMData(){
+  float sensor_volt; //Define variable for sensor voltage 
+  float RS_gas; //Define variable for sensor resistance  
+  float ratio; //Define variable for ratio
+  int sensorValue = analogRead(D33); //Read analog values of sensor  
+  Serial.print("SENSOR RAW VALUE = ");
+  Serial.println(sensorValue);
+  sensor_volt = sensorValue*(5.0/1023.0); //Convert analog values to voltage 
+  Serial.print("Sensor value in volts = ");
+  Serial.println(sensor_volt);
+  RS_gas = ((5.0*10.0)/sensor_volt)-10.0; //Get value of RS in a gas
+  Serial.print("Rs value = ");
+  Serial.println(RS_gas);
+  
+  ratio = RS_gas/calculateR0();  // Get ratio RS_gas/RS_air
+  
+  Serial.print("Ratio = ");
+  Serial.println(ratio);
+  float ppm_log = (log10(ratio)-b)/m; //Get ppm value in linear scale according to the the ratio value  
+  float ppm = pow(10, ppm_log); //Convert ppm value to log scale 
+  if(ppm < 100){
+    greenFlash();
+  }else{
+    if(ppm>100 && ppm<200){
+      yellowFlash();
+    }else if(ppm>200){
+      redFlash();
+    }
+  }
+  Serial.print("Our desired PPM = ");
+  Serial.println(ppm);
+  double percentage = ppm/10000; //Convert to percentage 
+  Serial.print("Value in Percentage = "); //Load screen buffer with percentage value
+  Serial.println(percentage); 
+  delay(1000);
+  return ppm;
+}
+
